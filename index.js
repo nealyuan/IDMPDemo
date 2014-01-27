ba2c9ada0f212b12e2590601538ad35d0f2d6eb7
Neal Yuan <yuanneal@gmail.com>
2014-01-26 21:39:58 -0800

    init

---
+++ index.js
@@ -0,0 +1,110 @@
+var express = require('express');
+var app = express();
+var fs = require ('fs');
+
+//configuration needed for using EJS
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'ejs');
+  app.use('/public', express.static(__dirname + '/public')); //public contains the style sheets
+  app.use(express.json());
+  app.use(express.urlencoded());
+});
+
+//Routes
+app.get('/', function(req, res) {  
+	fs.readFile ('fileUsage.csv', function (err, data){
+		if (err) throw err;
+		//data is buffer, therefore convert to String
+		var text = data.toString();
+		//Break up file into lines, an array with each element
+		//being a string of text
+		var lines = text.split(/\n|\r\n|\r/);
+
+		//Array of entry arrays
+		var entryArray = [];
+		for (var i = 0; i < lines.length; i++){
+			entryArray[i] = lines[i].split(',');
+		}
+
+	  res.render('index', {
+	    locals: {
+	      'title': 'UCLA Antimicrobial Guidelines Usage Data',
+	      'entryArray': entryArray
+	    }
+	  })
+	})
+})
+
+app.get('/vap', function(req, res) {  
+	fs.readFile ('VAPUsage.csv', function (err, data){
+		if (err) throw err;
+		//data is buffer, therefore convert to String
+		var text = data.toString();
+		//Break up file into lines, an array with each element
+		//being a string of text
+		var lines = text.split(/\n|\r\n|\r/);
+
+		//Array of entry arrays
+		var entryArray = [];
+		for (var i = 0; i < lines.length; i++){
+			entryArray[i] = lines[i].split(',');
+		}
+
+	  res.render('index', {
+	    locals: {
+	      'title': 'Ventilator-associated Pneumonia Guideline Usage',
+	      'entryArray': entryArray
+	    }
+	  })
+	})
+})
+
+app.get('/hap', function(req, res) {  
+	fs.readFile ('HAPUsage.csv', function (err, data){
+		if (err) throw err;
+		//data is buffer, therefore convert to String
+		var text = data.toString();
+		//Break up file into lines, an array with each element
+		//being a string of text
+		var lines = text.split(/\n|\r\n|\r/);
+
+		//Array of entry arrays
+		var entryArray = [];
+		for (var i = 0; i < lines.length; i++){
+			entryArray[i] = lines[i].split(',');
+		}
+	  res.render('index', {
+	    locals: {
+	      'title': 'Hospital Acquired Pneumonia Guideline Usage',
+	      'entryArray': entryArray
+	    }
+	  })
+	})
+})
+
+app.get('/sepsisFeedback', function(req, res) {  
+	fs.readFile ('sepsisFeedback.csv', function (err, data){
+		if (err) throw err;
+		//data is buffer, therefore convert to String
+		var text = data.toString();
+		//Break up file into lines, an array with each element
+		//being a string of text
+		var lines = text.split(/\n|\r\n|\r/);
+
+		//Array of entry arrays
+		var entryArray = [];
+		for (var i = 0; i < lines.length; i++){
+			entryArray[i] = lines[i].split(',');
+		}
+		
+	  res.render('index', {
+	    locals: {
+	      'title': 'Sepsis Guidelines User Feedback',
+	      'entryArray': entryArray
+	    }
+	  })
+	})
+})
+
+app.listen(process.env.PORT || 8000);
